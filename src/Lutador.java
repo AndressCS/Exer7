8c98525ab0c75f4609559143b4dd745446d2a864
Andress Silva <andresscvlcnt@gmail.com>
2024-02-15 21:48:48 -0300

    Update

---
+++ src/Lutador.java
@@ -0,0 +1,29 @@
+public class Lutador {
+    //Atributos
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+    public void apresentar(){
+
+    }
+    public void status(){
+
+    }
+    public void ganharLuta(){
+
+    }
+    public void perderLuta(){
+
+    }
+    public void empatarLuta(){
+
+    }
+
+}
